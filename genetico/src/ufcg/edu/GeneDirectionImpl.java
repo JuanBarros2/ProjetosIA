f5f1c4a777cf734b1fda0eafcdff7d509a039378
Juan Barros <juan.barros@ccc.ufcg.edu.br>
2018-11-07 20:32:59 -0300

    Refatora classe de mutação

--- genetico/src/ufcg/edu/GeneDirectionImpl.java
+++ genetico/src/ufcg/edu/GeneDirectionImpl.java
@@ -7,19 +7,18 @@ public class GeneDirectionImpl implements Gene {
     private Integer force;
     private Integer MAX_FORCE = 400;
     private Integer RATE_OF_RANDOM_MUTATION = Utils.LOW_MUTATION_RATE;
-    private Integer RATE_OF_MEDIUM_MUTATION = Utils.RATE_OF_MEDIUM_MUTATION;
+    private Integer RATE_OF_MEDIUM_MUTATION = Utils.MED_MUTATION_RATE;
     private Integer RATE_OF_SMALL_MUTATION = Utils.HIGH_MUTATION_RATE;
     private Integer SMALL_MUTATION_VARIATION = 30;
     private Integer MED_MUTATION_VARIATION = 70;
 
     @Override
     public boolean doMutation() {
-        bollean mutated = randomMutation() | mediumMutation() | smallMutation();
-        return mutated;
+        return randomMutation() |
+                mutationWithInterval(MED_MUTATION_VARIATION) |
+                mutationWithInterval(SMALL_MUTATION_VARIATION) ;
     }
-
-
-
+    
     private boolean randomMutation() {
         if (Utils.willMutate(RATE_OF_RANDOM_MUTATION)) {
             Random random = new Random();
@@ -29,18 +28,9 @@ public class GeneDirectionImpl implements Gene {
         return false;
     }
 
-    private boolean mediumMutation() {
+    private boolean mutationWithInterval(Integer rate) {
         if (Utils.willMutate(RATE_OF_MEDIUM_MUTATION)) {
-            Integer change = Utils.generateRandominInterval(MED_MUTATION_VARIATION);
-            setForce(this.force + change);
-            return true;
-        }
-        return false;
-    }
-
-    private boolean smallMutation() {
-        if (Utils.willMutate(RATE_OF_SMALL_MUTATION)) {
-            Integer change = Utils.generateRandominInterval(SMALL_MUTATION_VARIATION);
+            Integer change = Utils.generateRandominInterval(rate);
             setForce(this.force + change);
             return true;
         }
